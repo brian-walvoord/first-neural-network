0a387d4bc3d307dd3a90bd7aa4fcf3c626c6800a
Brian Walvoord <brian.walvoord@gmail.com>
2024-02-12 12:51:28 -0600

    install modules

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+import numpy as np
+import matplotlib
+
+print("Python: ", sys.version)
+print("Numpy: ", np.__version__)
+print("Matplotlib: ", matplotlib.__version__)
+
+# input layer -- 2nd hidden layer -- 3rd hidden layer -- output layer
