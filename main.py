4b77703481f077c9ae0742da6d630dd18301acd1
Brian Walvoord <brian.walvoord@gmail.com>
2024-02-13 10:25:07 -0600

    add layer to model

--- main.py
+++ main.py
@@ -9,11 +9,23 @@ print("Matplotlib: ", matplotlib.__version__)
 '''
 
 # input layer -- 2nd hidden layer -- 3rd hidden layer -- output layer
-inputs = [2.3, 4.5, 1.2]
-weights = [6.6, 3.4, 1.2]
-bias = 3
+inputs = [1, 2, 3, 2.5]
+
+weights1 = [0.2, 0.8, -0.5, 1]
+weights2 = [0.5, -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
+
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
 
 
+output = [inputs[0] * weights1[0] + inputs[1] * weights1[1] + inputs[2] * weights1[2] + inputs[3] * weights1[3] + bias1,
+          inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + inputs[3] * weights2[3] + bias2,
+          inputs[0] * weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] + inputs[3] * weights3[3] + bias3]
+print(output)
+
+'''
 def compute():
     total = 0
     # total = bias  <---- this causes large decimal when removing lines 18 and 24
@@ -27,3 +39,4 @@ def compute():
 
 output = compute()
 print(output)
+'''
